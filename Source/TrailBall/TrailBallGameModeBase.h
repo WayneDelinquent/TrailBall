a12181120c4e7ec608ebd712049aadf2d7455f67
Wayne Steedman <wayne@delinquent.io>
2021-09-13 19:49:49 +0200

    Initial Commit

---
+++ Source/TrailBall/TrailBallGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrailBallGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAILBALL_API ATrailBallGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
